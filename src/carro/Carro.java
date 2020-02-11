5025a22bcd4c8de161d26b4477abfc94130f0f49
nataliarosa88 <nataliag1988@gmail.com>
2020-02-10 22:35:42 -0300

    resolucao exercicio carro

--- src/carro/Carro.java
+++ src/carro/Carro.java
@@ -1,4 +1,45 @@
 package carro;
 
 public class Carro {
+
+    private String modelo;
+    private double potenciaDoMotor;
+    private String cor;
+
+    public Carro() {
+
+        super();
+    }
+
+    public Carro(String modelo, double potenciaDoMotor, String cor) {
+        super();
+        this.modelo = modelo;
+        this.potenciaDoMotor = potenciaDoMotor;
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPotenciaDoMotor() {
+        return potenciaDoMotor;
+    }
+
+    public void setPotenciaDoMotor(double potenciaDoMotor) {
+        this.potenciaDoMotor = potenciaDoMotor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 }
--- src/carro/Garagem.java
+++ src/carro/Garagem.java
@@ -1,4 +1,34 @@
 package carro;
 
 public class Garagem {
+
+    private static Carro carroPasseio;
+    private static Carro carroUtilitario;
+    private static Garagem garagem;
+
+    public static void main(String[] args) {
+
+        carroPasseio = new Carro();
+        carroUtilitario = new Carro();
+
+        garagem = new Garagem();
+
+        garagem.carroPasseio.setCor("azul");
+        garagem.carroPasseio.setModelo("uno");
+        garagem.carroPasseio.setPotenciaDoMotor(1.0);
+
+        garagem.carroUtilitario.setCor("azul");
+        garagem.carroUtilitario.setModelo("uno");
+        garagem.carroUtilitario.setPotenciaDoMotor(1.0);
+
+        System.out.println("Carro de Passeio: ");
+        System.out.println("Cor: " + garagem.carroPasseio.getCor());
+        System.out.println("Modelo: " + garagem.carroPasseio.getModelo());
+        System.out.println("Potência: " + garagem.carroPasseio.getPotenciaDoMotor());
+
+        System.out.println("Carro de Utilitário: ");
+        System.out.println("Cor: " + garagem.carroUtilitario.getCor());
+        System.out.println("Modelo: " + garagem.carroUtilitario.getModelo());
+        System.out.println("Potência: " + garagem.carroUtilitario.getPotenciaDoMotor());
+    }
 }
